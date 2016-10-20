bb90ecdd4ea9d991e91db38d89ad3d6d68ed041d
Xuefeng Ding <xuefeng.ding@gssi.infn.it>
2016-10-20 16:00:36 +0200

    remove irrelavant files

--- UniformDithering.hh
+++
@@ -1,9 +0,0 @@
-#ifndef UniformDithering_H
-#define UniformDithering_H
-#include "IDithering.hh"
-namespace GeSpcAnalysis {
-	 class UniformDithering : public IDithering {
-		  const double Dither() const;
-	 };
-}
-#endif
